c24d7c53701c54c97235fc96e486593c0c54104d
Peter Heumader <iisjku@gmail.com>
2019-05-07 12:32:33 +0200

    ^https google callback

--- portal/config/config.js
+++ portal/config/config.js
@@ -1,6 +1,12 @@
+let serverURL = "";
+
+if(process.env["SERVER_URL"]){
+    serverURL = process.env["SERVER_URL"];
+}
+
 module.exports = {
     google : {
-        callbackURL: "/auth/callback",
+        callbackURL: serverURL+"/auth/callback",
         clientID: "182795011122-jgj96rhi5chbflic65bsaro75i56u4r7.apps.googleusercontent.com",
         clientSecret: "nHYnwqLwpXu22mcQ0u4U_dy4",
         scope: ["profile", "email","openid"]
